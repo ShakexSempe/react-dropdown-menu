642717666c7813d5fb9508b2df7e5cad449182ec
Ray-Shakex <bshakex@gmail.com>
2021-11-27 11:33:12 +0200

    setup files and navbar setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar>
+      <NavItem icon="&#128512;"/>
+      <NavItem icon="&#128525;"/>
+      <NavItem icon="&#128151;"/>
+    </Navbar>
   );
+}
+
+function Navbar(props) {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">
+        { props.children }
+      </ul>
+    </nav>
+  )
+}
+
+function NavItem(props) {
+  return (
+    <li className="nav-item">
+      <a href="#" className="icon-button">
+        {props.icon}
+      </a>
+    </li>
+  )
 }
 
 export default App;
