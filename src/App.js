b98b67eca96e46d7b4d787e0b02393c1bc024f83
Ray-Shakex <bshakex@gmail.com>
2021-11-28 11:10:46 +0200

    navbar svg icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { ReactComponent as BellIcon } from './icons/bell.svg';
+import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
+import { ReactComponent as CaretIcon } from './icons/caret.svg';
+import { ReactComponent as PlusIcon } from './icons/plus.svg';
+import { ReactComponent as CogIcon } from './icons/cog.svg';
+import { ReactComponent as ChevronIcon } from './icons/chevron.svg';
+import { ReactComponent as BoltIcon } from './icons/bolt.svg';
 
 function App() {
   return (
     <Navbar>
-      <NavItem icon="&#128512;"/>
-      <NavItem icon="&#128525;"/>
-      <NavItem icon="&#128151;"/>
+      <NavItem icon={<PlusIcon />}/>
+      <NavItem icon={<BellIcon />}/>
+      <NavItem icon={<MessengerIcon />}/>
     </Navbar>
   );
 }
