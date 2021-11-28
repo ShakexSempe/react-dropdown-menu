9c50ae62d7720c656e3e2ff93be274237d10d944
Ray-Shakex <bshakex@gmail.com>
2021-11-28 11:28:25 +0200

    nav-item useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { ReactComponent as BellIcon } from './icons/bell.svg';
 import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
 import { ReactComponent as CaretIcon } from './icons/caret.svg';
@@ -7,12 +6,20 @@ import { ReactComponent as CogIcon } from './icons/cog.svg';
 import { ReactComponent as ChevronIcon } from './icons/chevron.svg';
 import { ReactComponent as BoltIcon } from './icons/bolt.svg';
 
+import React, { useState } from 'react';
+
+
 function App() {
   return (
     <Navbar>
       <NavItem icon={<PlusIcon />}/>
       <NavItem icon={<BellIcon />}/>
       <NavItem icon={<MessengerIcon />}/>
+
+      <NavItem icon={<CaretIcon />}>
+        {/* dropdown goes here */}
+
+      </NavItem>
     </Navbar>
   );
 }
@@ -28,11 +35,16 @@ function Navbar(props) {
 }
 
 function NavItem(props) {
+
+  const [open, setOpen] = useState(false);
+
   return (
     <li className="nav-item">
-      <a href="#" className="icon-button">
+      <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
         {props.icon}
       </a>
+
+      {open && props.children}
     </li>
   )
 }
